3d31cff5cc9ab6faaf58dd0912f0d6f7757bbd83
Flutter PC <sina.sam@gmail.com>
2022-10-23 13:04:30 +0100

    dumy data

--- Data/mealJson.js
+++ Data/mealJson.js
@@ -1,11 +1,38 @@
-const mealObject = {
-    "mealName" : "Fesenjoon",
-    "heroImg" : "/public/assets/fes.webp",
-    "cookingTime" : {"date" : "23/11/2022", "time" : "8:00"},
-    "neighborhood" : "hendon",
-    "totalSum" : 20,
-    "orderSum" : 14,
-    "infoText" : "Fesenjān is flavored with pomegranate paste and ground walnuts and spices like turmeric, cinnamon, orange peel, cardamom, and rosebud. It is traditionally made with eggplant and poultry (duck or chicken). Fesenjān can also be made using balls of ground meat or chunks of lamb. Depending on the recipe, it can have a sweet or sour taste. Fesenjān is served with Iranian white or yellow rice (polo or chelo).",
-    "images" : ["/public/assets/ghe.webp","/public/assets/tah.webp","/public/assets/ash.webp"],
-    "videos" : ["https://www.youtube.com/watch?v=qwKpEnqh_88"]
-}
+const mealObject = [
+        {
+        "id" : 001,
+        "mealName" : "Fesenjoon",
+        "heroImg" : "/public/assets/fes.webp",
+        "cookingTime" : {"date" : "23/11/2022", "time" : "8:00"},
+        "neighborhood" : "hendon",
+        "totalSum" : 20,
+        "orderSum" : 14,
+        "infoText" : "Fesenjān is flavored with pomegranate paste and ground walnuts and spices like turmeric, cinnamon, orange peel, cardamom, and rosebud. It is traditionally made with eggplant and poultry (duck or chicken). Fesenjān can also be made using balls of ground meat or chunks of lamb. Depending on the recipe, it can have a sweet or sour taste. Fesenjān is served with Iranian white or yellow rice (polo or chelo).",
+        "images" : ["/public/assets/ghe.webp","/public/assets/tah.webp","/public/assets/ash.webp"],
+        "videos" : ["https://www.youtube.com/watch?v=qwKpEnqh_88"]
+    },
+    {
+        "id" : 002,
+        "mealName" : "Gheymeh",
+        "heroImg" : "/public/assets/ghe.webp",
+        "cookingTime" : {"date" : "24/11/2022", "time" : "9:00"},
+        "neighborhood" : "hendon",
+        "totalSum" : 30,
+        "orderSum" : 25,
+        "infoText" : "Fesenjān is flavored with pomegranate paste and ground walnuts and spices like turmeric, cinnamon, orange peel, cardamom, and rosebud. It is traditionally made with eggplant and poultry (duck or chicken). Fesenjān can also be made using balls of ground meat or chunks of lamb. Depending on the recipe, it can have a sweet or sour taste. Fesenjān is served with Iranian white or yellow rice (polo or chelo).",
+        "images" : ["/public/assets/ghe.webp","/public/assets/tah.webp","/public/assets/ash.webp"],
+        "videos" : ["https://www.youtube.com/watch?v=qwKpEnqh_88"]
+    },
+    {
+        "id" : 003,
+        "mealName" : "Ash reshteh",
+        "heroImg" : "/public/assets/ash.webp",
+        "cookingTime" : {"date" : "25/11/2022", "time" : "13:00"},
+        "neighborhood" : "finchly",
+        "totalSum" : 40,
+        "orderSum" : 36,
+        "infoText" : "Fesenjān is flavored with pomegranate paste and ground walnuts and spices like turmeric, cinnamon, orange peel, cardamom, and rosebud. It is traditionally made with eggplant and poultry (duck or chicken). Fesenjān can also be made using balls of ground meat or chunks of lamb. Depending on the recipe, it can have a sweet or sour taste. Fesenjān is served with Iranian white or yellow rice (polo or chelo).",
+        "images" : ["/public/assets/ghe.webp","/public/assets/tah.webp","/public/assets/ash.webp"],
+        "videos" : ["https://www.youtube.com/watch?v=qwKpEnqh_88"]
+    },
+]
