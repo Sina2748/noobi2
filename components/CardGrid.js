29485f44e2a60921dac32913e247840bca06b6f9
Flutter PC <sina.sam@gmail.com>
2022-10-24 19:47:22 +0100

    test 2

--- components/CardGrid.js
+++ components/CardGrid.js
@@ -12,7 +12,7 @@ function CardGrid (props) {
       <div className="max-w-screen-md mb-8 lg:mb-16">
           <h2 className="mb-4 ml-6 md:ml-0 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Pick and Order one of our Meal Deals</h2>
       </div>
-      <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+      {/* <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
             {mealJson.map((meal) => 
                   
                   
@@ -29,14 +29,14 @@ function CardGrid (props) {
                   time={meal.cookingTime.time}
                   mealsLeft={meal.totalSum - meal.orderSum}
                   neighborhood={meal.neighborhood}
-/>
+                  />
             </div>
                   
              )} 
 
 
 
-      </div>
+      </div> */}
   </div>
 </section>
 
